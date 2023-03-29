281cc5a8cdc8cb5397f06b5cc24a01d6e027979f
Nicholas Wandinata <116739722+NWandinata@users.noreply.github.com>
2023-03-28 23:06:31 -0400

    Established simple movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    
+    // Runs when game starts (i.e. script is loaded)
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Runs every frame to change player's movement direction
+    private void Update()
+    {
+        // Updates horizontal movement (vertical is kept the same)
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        // Jump when space is pressed
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+}
