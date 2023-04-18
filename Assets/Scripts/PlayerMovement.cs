cd25fd6f8e65a5543d641142c44379abb8d68386
Nicholas Wandinata <116739722+NWandinata@users.noreply.github.com>
2023-04-18 12:43:37 -0400

    Fixed Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
-    
+    private bool grounded;
+
     // Runs when game starts (i.e. script is loaded)
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
-    
+
     // Runs every frame to change player's movement direction
     private void Update()
     {
@@ -18,7 +19,19 @@ public class PlayerMovement : MonoBehaviour
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
         // Jump when space is pressed
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
