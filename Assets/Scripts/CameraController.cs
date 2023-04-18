3794e2caa8c978c0d54da491dc12dc3803c9d267
Nicholas Wandinata <116739722+NWandinata@users.noreply.github.com>
2023-04-18 14:56:28 -0400

    Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private void Update()
+    {
+        // Sub in player.position.y to follow vertically
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
