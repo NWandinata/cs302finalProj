45eef392adc43a8ff84bcaade4e55a9b99180284
Makenzie Johnson <mvolleyballjohnson@gmail.com>
2023-04-18 22:31:50 -0400

    Finished the basics of Damage
    Can fix later when adding more obstacles.

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    //public Renderer change;
     public SpriteRenderer player;
-    [SerializeField] private Color[] colors; //yellow and red
+    //private List<Color> colors;
+    public int life = 3;
 
     // Start is called before the first frame update
     void Start() //make an obstacle to hit
     {
-        player.GetComponent<SpriteRenderer>().color = Color.yellow;
+        player = GetComponent<SpriteRenderer>();
+        /*colors = new List<Color>();
+        colors.Add(Color.green);
+        colors.Add(Color.yellow);
+        colors.Add(Color.red);*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if a certain button is hit (aka. player gets hit), call Damage_Taken
+        
     }
 
-    public void Damage_Taken()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.name == "Obstacle")
+        {
+            Damage_Taken(player);
+        }
+    }
 
+    public void Damage_Taken(SpriteRenderer play)
+    {
+        life--;
+        if (life == 2)
+        {
+            play.GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+
+        else if (life == 1)
+        {
+            play.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
+        else if (life <= 0)
+        {
+            Destroy(play);
+            //maybe restart level
+        }
     }
 }
