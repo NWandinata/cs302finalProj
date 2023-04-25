0ca63d57d62ea05ef99415f817d9bf98fc70feac
Nicholas Wandinata <116739722+NWandinata@users.noreply.github.com>
2023-04-25 14:12:46 -0400

    Update ShopLoader.cs

--- Assets/Scripts/ShopLoader.cs
+++ Assets/Scripts/ShopLoader.cs
@@ -5,30 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class ShopLoader : MonoBehaviour
 {
-    private bool playerInZone;
     public string levelToLoad;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerInZone = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerInZone)
-        {
-            SceneManager.LoadScene(levelToLoad);
-        }
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
         {
-            playerInZone = true;
+            SceneManager.LoadScene(levelToLoad);
         }
     }
 }
-
