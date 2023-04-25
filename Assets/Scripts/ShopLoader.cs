2430c8113a1cb32f98cd6759f4098c2bb06f45fe
galderso <gmalderson4321@yahoo.com>
2023-04-25 16:00:47 -0400

    Revert "Update ShopLoader.cs"
    This reverts commit 0ca63d57d62ea05ef99415f817d9bf98fc70feac.

--- Assets/Scripts/ShopLoader.cs
+++ Assets/Scripts/ShopLoader.cs
@@ -5,13 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class ShopLoader : MonoBehaviour
 {
+    private bool playerInZone;
     public string levelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInZone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInZone)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
         {
-            SceneManager.LoadScene(levelToLoad);
+            playerInZone = true;
         }
     }
 }
+
