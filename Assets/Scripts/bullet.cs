806c0c860b389dd055a40fd0d7de79a94f7234b7
Nicholas Wandinata <116739722+NWandinata@users.noreply.github.com>
2023-05-08 17:18:14 -0400

    Update bullet.cs

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -12,7 +12,7 @@ public class bullet : MonoBehaviour
     private float lifetime;
     private BoxCollider2D boxCollider;
 
-    // Start is called before the first frame update
+    // Gives the bullet collision
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -20,36 +20,49 @@ public class bullet : MonoBehaviour
     //update after hit
     private void Update()
     {
+        // Speed and lifetime isn't calculated for a bullet that has collided since it will become inactive
         if (hit) return;
+
         float travelSpeed = speed * Time.deltaTime * direction;
         transform.Translate(travelSpeed, 0, 0);
 
+        // Bullet will become inactive after 4 seconds of travel without collision
         lifetime += Time.deltaTime;
         if (lifetime > 4) gameObject.SetActive(false);
     }
 
-    private void OnTriggerEnter2D(Collider2D hitInfo) //when the enemy gets hit
+    // Detects collision with any object
+    private void OnTriggerEnter2D(Collider2D hitInfo)
     {
         hit = true;
         enemy Enemy = hitInfo.GetComponent<enemy>();
 
+        // Bullets will damage enemies
         if (Enemy != null)
         {
             Enemy.TakeDamage(damage);
         }
 
+        // Bullet becomes inactive and its collision disappears upon hit
         boxCollider.enabled = false;
         gameObject.SetActive(false);
     }
+<<<<<<< Updated upstream
     //sets bullet direction
+=======
+
+    // Bullets will always travel right (forward from player)
+>>>>>>> Stashed changes
     public void SetDirection(float bulletDirection)
     {
+        // Sets the bullet as an active object
         lifetime = 0;
         direction = bulletDirection;
         gameObject.SetActive(true);
         hit = false;
         boxCollider.enabled = true;
 
+        // Makes bullet travel in front of the player
         float localScaleX = transform.localScale.x;
         if (Mathf.Sign(localScaleX) != bulletDirection)
             localScaleX = -localScaleX;
